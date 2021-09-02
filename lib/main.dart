99bf3059733a7ee28bc1e4deee52b7d1b8fb320f
‘kailus’ <kailusdev@gmail.com>
2021-09-02 11:45:17 -0500

    Widget Slide

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/slidetosubmit_widget.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Slide App',
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SlideToSubmitWidget(
+                color: Colors.blueGrey,
+                text: 'Slide to Submit...',
+                icon: Icons.arrow_right_alt,
+                onSubmit: (onFinish, onError) async {
+                  onFinish();
+                }
+              ),
+              SizedBox(height: 15,),
+              SlideToSubmitWidget(
+                color: Colors.blue,
+                text: 'GO on',
+                icon: Icons.arrow_right_alt,
+                onSubmit: (onFinish, onError) async {
+                  onError();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
